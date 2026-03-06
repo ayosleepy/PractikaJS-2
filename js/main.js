3e31948fd1f3cd88fac1911a43c5809c815654b9
ayosleepy <filippmello2007@gmail.com>
2026-03-06 09:15:06 +0700

    addNewCard method

--- js/main.js
+++ js/main.js
@@ -4,5 +4,19 @@ let app = new Vue({
         column1: [],
         column2: [],
         column3: []
+    },
+    methods: {
+    addNewCard() {
+        let newCard = {
+            id: Date.now(),
+            title: 'New note',
+            items: [
+                { text: 'Task 1', done: false },
+                { text: 'Task 2', done: false },
+                { text: 'Task 3', done: false }
+            ]
+        }
+        this.column1.push(newCard)
     }
+},
 })
